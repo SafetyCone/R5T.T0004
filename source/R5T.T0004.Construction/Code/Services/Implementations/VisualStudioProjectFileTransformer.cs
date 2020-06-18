34124ec843c35c69a50969fc101a3998672d8382
MinexAutomation <minex.automation@gmail.com>
2020-06-18 16:52:27 -0700

    Fix to use assignment semantics for Settable<T>'s.

--- source/R5T.T0004.Construction/Code/Services/Implementations/VisualStudioProjectFileTransformer.cs
+++ source/R5T.T0004.Construction/Code/Services/Implementations/VisualStudioProjectFileTransformer.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 
 using R5T.T0002;
-using R5T.T0003;
 
 
 namespace R5T.T0004.Construction
@@ -11,13 +10,13 @@ namespace R5T.T0004.Construction
     {
         public Task CopySourceToDestinationAsync(IVisualStudioProjectFile source, IVisualStudioProjectFile destination)
         {
-            destination.GenerateDocumentationFile.CloneFrom(source.GenerateDocumentationFile);
-            destination.IsPackable.CloneFrom(source.IsPackable);
-            destination.LanguageVersion.CloneFrom(source.LanguageVersion);
-            destination.NoWarn.CloneFrom(source.NoWarn);
-            destination.OutputType.CloneFrom(source.OutputType);
-            destination.SDK.CloneFrom(source.SDK);
-            destination.TargetFramework.CloneFrom(source.TargetFramework);
+            destination.GenerateDocumentationFile = source.GenerateDocumentationFile;
+            destination.IsPackable = source.IsPackable;
+            destination.LanguageVersion = source.LanguageVersion;
+            destination.NoWarn = source.NoWarn;
+            destination.OutputType = source.OutputType;
+            destination.SDK = source.SDK;
+            destination.TargetFramework = source.TargetFramework;
 
             destination.ClonePackageReferencesFrom(source);
             destination.CloneProjectReferencesFrom(source);
