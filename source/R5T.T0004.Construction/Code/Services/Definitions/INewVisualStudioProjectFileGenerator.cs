e419144dc37d3c646ede7233e783859b32a743ef
MinexAutomation <minex.automation@gmail.com>
2020-06-18 16:51:41 -0700

    Added INewVisualStudioProjectFileGenerator service and NewXDocumentVisualStudioProjectFileGenerator implementation.

---
+++ source/R5T.T0004.Construction/Code/Services/Definitions/INewVisualStudioProjectFileGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0002;
+
+
+namespace R5T.T0004.Construction
+{
+    /// <summary>
+    /// Service to create new <see cref="IVisualStudioProjectFile"/> instances.
+    /// Hides the implementation type.
+    /// </summary>
+    public interface INewVisualStudioProjectFileGenerator
+    {
+        Task<IVisualStudioProjectFile> CreateNewVisualStudioProjectFile();
+    }
+}
