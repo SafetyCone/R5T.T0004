70fa36ca4d9e354c32ef13eaec2788b5611d2a88
MinexAutomation <minex.automation@gmail.com>
2020-06-18 17:14:34 -0700

    Added true IVisualStudioProjectFileSerializer service definition and VisualStudioProjectFileSerializer implementation that uses IVisualStudioProjectFileTransformer.

---
+++ source/R5T.T0004.Construction/Code/Services/Implementations/VisualStudioProjectFileSerializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0002;
+
+
+namespace R5T.T0004.Construction
+{
+    public class VisualStudioProjectFileSerializer : IVisualStudioProjectFileSerializer
+    {
+        private IVisualStudioProjectFileTransformer VisualStudioProjectFileTransformer { get; }
+        private IXDocumentVisualStudioProjectFileSerializer XDocumentVisualStudioProjectFileSerializer { get; }
+
+
+
+        public VisualStudioProjectFileSerializer(
+            IVisualStudioProjectFileTransformer visualStudioProjectFileTransformer,
+            IXDocumentVisualStudioProjectFileSerializer xDocumentVisualStudioProjectFileSerializer)
+        {
+            this.XDocumentVisualStudioProjectFileSerializer = xDocumentVisualStudioProjectFileSerializer;
+        }
+
+        public async Task<IVisualStudioProjectFile> DeserializeAsync(string projectFilePath)
+        {
+            var visualStudioProjectFile = await this.XDocumentVisualStudioProjectFileSerializer.DeserializeAsync(projectFilePath);
+            return visualStudioProjectFile;
+        }
+
+        public async Task SerializeAsync(string projectFilePath, IVisualStudioProjectFile visualStudioProjectFile, bool overwrite = true)
+        {
+            XDocumentVisualStudioProjectFile xDocumentVisualStudioProjectFile;
+
+            var isXDocumentVisualStudioProjectFile = visualStudioProjectFile is XDocumentVisualStudioProjectFile;
+            if(isXDocumentVisualStudioProjectFile)
+            {
+                xDocumentVisualStudioProjectFile = visualStudioProjectFile as XDocumentVisualStudioProjectFile;
+            }
+            else
+            {
+                // If not, create a new XDocument Visual Studio Project File instance, then transform it into the input instance.
+                xDocumentVisualStudioProjectFile = XDocumentVisualStudioProjectFile.New();
+
+                await this.VisualStudioProjectFileTransformer.CopySourceToDestinationAsync(visualStudioProjectFile, xDocumentVisualStudioProjectFile);
+            }
+
+            await this.XDocumentVisualStudioProjectFileSerializer.SerializeAsync(projectFilePath, xDocumentVisualStudioProjectFile, overwrite);
+        }
+    }
+}
